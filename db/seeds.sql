a801ff579c96c1d27672310330a9f221af275745
Dallas Moore <moore.johndallas@gmail.com>
2023-06-06 15:02:56 -0500

    added MySQL connection and connected to database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,33 @@
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'Violet', 'Turner', 1) --add manager id later
-       (2, 'Lionel', 'Cross', 2)
-       (3, 'Phillipe', 'Hornesworth', 2)
-       (4, 'Oakley', 'Cedar', 3)
-       (5, 'Daphne', 'Delaney', 4)
-       (6, 'Drew', 'Appleton', 4)
-       (7, 'Scarlette', 'Andrews', 5)
-       (8, 'Robin', 'Lueffner', 6)
-       (9, 'Chet', 'Fallen', 6)
-       (10, 'Urvin', 'Wright', 7)
-       (11, 'Marques', 'Sloan', 8)
-       (12, 'Wayne', 'Hammond', 8)
+INSERT INTO employees (id, first_name, last_name, role_id)
+VALUES (1, 'Violet', 'Turner', 1),
+       (2, 'Lionel', 'Cross', 2),
+       (3, 'Phillipe', 'Hornesworth', 2),
+       (4, 'Oakley', 'Cedar', 3),
+       (5, 'Daphne', 'Delaney', 4),
+       (6, 'Drew', 'Appleton', 4),
+       (7, 'Scarlette', 'Andrews', 5),
+       (8, 'Robin', 'Lueffner', 6),
+       (9, 'Chet', 'Fallen', 6),
+       (10, 'Urvin', 'Wright', 7),
+       (11, 'Marques', 'Sloan', 8),
+       (12, 'Wayne', 'Hammond', 8);
 
 
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (1, 'Sales Lead', 100000, 1)
-       (2, 'Salesperson', 75000, 1)
-       (3, 'Account Manager', 160000, 2)
-       (4, 'Accountant', 125000, 2)
-       (5, 'Legal Team Lead', 250000, 3)
-       (6, 'Lawyer', 190000, 3)
-       (7, 'Lead Engineer', 150000, 4)
-       (8, 'Software Engineer', 11100, 4) 
+VALUES (1, 'Sales Lead', 100000, 1),
+       (2, 'Salesperson', 75000, 1),
+       (3, 'Account Manager', 160000, 2),
+       (4, 'Accountant', 125000, 2),
+       (5, 'Legal Team Lead', 250000, 3),
+       (6, 'Lawyer', 190000, 3),
+       (7, 'Lead Engineer', 150000, 4),
+       (8, 'Software Engineer', 110000, 4); 
 
 
 
 INSERT INTO department (id, name)
-VALUES (1, 'Sales')
-       (2, 'Finance')
-       (3, 'Legal')
-       (4, 'Engineering') 
+VALUES (1, 'Sales'),
+       (2, 'Finance'),
+       (3, 'Legal'),
+       (4, 'Engineering'); 
--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const clogIntro = `
-#  ███████╗███╗   ███╗██████╗ ██╗      ██████╗ ██╗   ██╗███████╗███████╗
-#  ██╔════╝████╗ ████║██╔══██╗██║     ██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝
-#  █████╗  ██╔████╔██║██████╔╝██║     ██║   ██║ ╚████╔╝ █████╗  █████╗  
-#  ██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██╔══╝  ██╔══╝  
-#  ███████╗██║ ╚═╝ ██║██║     ███████╗╚██████╔╝   ██║   ███████╗███████╗
-#  ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝
-#                                                                       
-#  ████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗             
-#  ╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗            
-#     ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝            
-#     ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗            
-#     ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║            
-#     ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝            
-#                                                                       
-`;
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
-console.log(clogIntro);
+// Create a MySQL connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'employee_db',
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the employee database.');
+
+  // Start the application
+  start();
+});
+
+// Function to start the application
+
+// Function to view all departments
+
+// Function to view all roles
+
+// Function to view all employees
+
+// Function to add a department
+
+// Function to add a role
+
+// Function to add an employee
+
+// Function to update an employee's role
