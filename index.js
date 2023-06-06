9a3758f2bb3c33ef6f83c1da13554f5fc201b2bd
Dallas Moore <moore.johndallas@gmail.com>
2023-06-06 18:06:14 -0500

    moved code in questions.js file back into index.js file, added function to view all departments

--- index.js
+++ index.js
@@ -19,17 +19,76 @@ db.connect((err) => {
 });
 
 // Function to start the application
-
-// Function to view all departments
-
+function start(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Exit',
+                ],
+            },
+        ])
+        .then((answers) => {
+            switch (answers.action) {
+                case 'View all departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all roles':
+                    viewAllRoles();
+                    break;
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Exit':
+                    db.end();
+                    break;
+            }
+        });
+    };
+        // Function to view all departments
+        function viewAllDepartments() {
+            db.query('SELECT * FROM department', (err,results) => {
+                if (err) throw err;
+                
+                console.table(results);
+                start();
+            });
+        };
 // Function to view all roles
+// viewAllRoles(){
 
-// Function to view all employees
+// };
+// // Function to view all employees
+// viewAllEmployees(){
 
-// Function to add a department
+// };
+// // Function to add a department
+// addDepartment(){
 
-// Function to add a role
+// };
+// // Function to add a role
+// addRole(){
 
-// Function to add an employee
+// };
+// // Function to add an employee
+// addEmployee(){
 
-// Function to update an employee's role
+// };
